1694e10b222f351e686ef0de74789ace02a19ecb
nstrohmayer <Strohmayer.Noah@hotmail.com>
2021-04-25 21:12:23 +0200

    kinda working Application.kt

--- calculation/src/main/kotlin/io/cloudflight/application/Application.kt
+++ calculation/src/main/kotlin/io/cloudflight/application/Application.kt
@@ -1,13 +1,27 @@
 package io.cloudflight.application
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import io.cloudflight.converter.toInputParameter
+import io.cloudflight.usecase.PlotAbstractnessInstabilityGraph
+import io.cloudflight.usecase.PlotAbstractnessInstabilityGraphImpl
 
-@SpringBootApplication
-open class Application
+//class Application {
 
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+object App {
+    const val appName = "DBS CLI App"
+    const val version = "0.0.1"
 }
 
+private val plotAbstractnessInstabilityGraph: PlotAbstractnessInstabilityGraph =
+    PlotAbstractnessInstabilityGraphImpl()
 
+// TODO use https://github.com/Kotlin/kotlinx-cli for smoother cli support
+fun main(args: Array<String>) {
+    try {
+        val inputParameters = args.toInputParameter()
+
+        plotAbstractnessInstabilityGraph.plotAbstractnessInstabilityGraph(inputParameter = inputParameters)
+    } catch (exception: Exception) {
+        println(exception.message)
+    }
+}
+//}
